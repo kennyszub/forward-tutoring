85a941acadf04049c926d6d181773bfe8c25d243
saasbook <saasbook@saasbook.(none)>
2013-10-28 00:55:16 -0700

    show user page working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_filter :save_login_state, :only => [:new, :create]
+  before_filter :authenticate_user, :only => [:show]
 
   def new
     @user = User.new
@@ -10,11 +11,17 @@ class UsersController < ApplicationController
     if @user.save
       flash[:notice] = "You signed up successfully"
       flash[:color]= "valid"
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
     else
       flash[:notice] = "Form is invalid"
       flash[:color]= "invalid"
+      render "new"
     end
-    #render "new"
-    redirect_to '/'
+    #redirect_to '/'
+  end
+
+  def show
+
   end
 end
